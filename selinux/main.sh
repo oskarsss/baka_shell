9fe760540226aa71d30988bba374db61804aaefd
flusher998 <jolka28485343@gmail.com>
2022-04-26 07:09:06 +0300

    added parametrs like allow-app enable-enforce enable-enforce-perm

--- selinux/main.sh
+++ selinux/main.sh
@@ -1,18 +1,56 @@
 #!/bin/bash
 
 if "$1" = "relabel"
-	then
+then
 	echo "after relabel in order to add DE to allowed policy launch command this program with parametr allow-de"
+	echo "but now - reboot"
 	sudo fixfiles –F onboot
 	sudo grep -rl 'SELINUX=enforcing' /etc/selinux/config | sudo xargs sed -i 's/SELINUX=enforcing/SELINUX=permissive/g'
 	sudo grep -rl 'SELINUX=disabled' /etc/selinux/config | sudo xargs sed -i 's/SELINUX=disabled/SELINUX=permissive/g'
-elif "$1"  = "allow_de"
-	then
+elif "$1"  = "allow-de"
+then
 	sudo allow2audit -a -M allow_de
 	sudo semodule -i allow_de.pp
-	mkdir /home/$user/selinux_policy
-	mv ./allow_de.* /home/$user/selinux_policy 
+	mkdir /home/$USER/selinux_policy
+	mv ./allow_de.* /home/$USER/selinux_policy 
 
+elif "$1" = "allow-app"
+then	
+
+	sudo cp /var/log/audit/audit.log /var/log/audit/audit.log.old
+
+	sudo setenforce 0
+
+	echo "Open app/s that are not working because of SELinux"
+
+	date=$(date +"%Hh-%Mm-%Ss-%dd-%mm-%yy")
+
+	read -p "When opened all needed apps, enter name of your chosen policy for these rules"$'\n' answer
+
+	answer="$answer-$date"
+
+	mkdir /home/$USER/selinux_policy
+
+	sudo awk 'NR==FNR{a[$0];next}!($0 in a)' /var/log/audit/audit.log.old /var/log/audit/audit.log > /home/$USER/selinux_policy/temp_audit_log.txt
+	sudo mv /home/$USER/selinux_policy/temp_audit_log.txt /var/log/audit/audit.log 
+	sudo chown root:root /var/log/audit/audit.log
+	sudo chmod 0640 /var/log/audit/audit.log
+
+	sudo allow2audit -a -M "$answer"
+
+	sudo semodule -i "$answer.pp"
+
+elif "$1" = "enable-enforce"
+then 
+	sudo setenforce 1 && echo "enforce mode enabled"
+
+elif "$1" = "enable-enforce-perm"
+then
+	echo "Do you really know what you are going to do?"
+##check for answers from the firewalld check
+	
+	sudo grep -rl 'SELINUX=permissive' /etc/selinux/config | sudo xargs sed -i 's/SELINUX=permissive/SELINUX=enforcing/g'
+	sudo grep -rl 'SELINUX=disabled' /etc/selinux/config | sudo xargs sed -i 's/SELINUX=disabled/SELINUX=enforcing/g'
 else 
 	echo "Wrong input try again"
 fi
@@ -24,15 +62,7 @@ fi
 
 ##sudo cp /var/log/audit/audit.log /var/log/audit/audit.log.old
 
-##sudo setenforce 0
 
-echo "Open app/s that are not working because of SELinux"
-
-date=$(date +"%Hh-%Mm-%Ss-%dd-%mm-%yy")
-
-read -p "When opened all needed apps, enter name of your chosen policy for these rules"$'\n' answer
-
-answer="$answer-$date"
 echo "$answer"
 
 
