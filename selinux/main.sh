f6177bc2d68d8607c840a9eafd7f5d560177b446
flusher998 <jolka28485343@gmail.com>
2022-04-26 05:42:48 +0300

    added some functionality to selinux main.sh

--- selinux/main.sh
+++ selinux/main.sh
@@ -1,3 +1,18 @@
-!#/bin/bash
+#!/bin/bash
+
+##some question about auto relabel and enabling DE
+
+##sudo cp /var/log/audit/audit.log /var/log/audit/audit.log.old
+
+##sudo setenforce 0
+
+echo "Open app/s that are not working because of SELinux"
+
+date=$(date +"%Hh-%Mm-%Ss-%dd-%mm-%yy")
+
+read -p "When opened all needed apps, enter name of your chosen policy for these rules"$'\n' answer
+
+answer="$answer-$date"
+echo "$answer"
 
 
---
+++ selinux/test.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+read -p "Your question here: "
+
+answer="$REPLY"
+
+echo "$answer"
